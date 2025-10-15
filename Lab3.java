9b40809492461582a82bcf61ed59ae7debc899e6
VixenLaRuk <vixenlaruk@gmail.com>
2025-10-15 14:33:10 -0700

    lab3 first commit

---
+++ Lab3.java
@@ -0,0 +1,14 @@
+/**
+ * This program consist of a guessing game and a rock-paper-scissor game.
+ * @author Vixen La Ruk
+ * @author Edisson Ajciginac
+ * @since 10/15/2025
+ */
+
+public class Lab3 {
+
+    /**
+     * The guessing game will go here
+     */
+    
+}
